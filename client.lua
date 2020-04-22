0ebf74bbc8688ea096fa05841acfca29853b9706
daZepelin <63473480+daZepelin@users.noreply.github.com>
2020-04-22 22:54:26 +0100

    Slight optimization. decreased speed limit delay.
    There were lots of if statements inside while loop which made the script to use lots client's resources, this is now changed and should work a bit lighter
    Decreased Speed limit delay, added delay configuration at the top of client.lua for easier access

--- client.lua
+++ client.lua
@@ -4,6 +4,7 @@ local justIn = false
 local tParams = {tyresPopped = 0, isSpeedLimited = false, t0 = false, t1 = false, t4 = false, t5 = false}
 local speedLimitTwoTires = 12.0  --Speed limit when 2 or more tires get bursted
 local speedLimitOneTire = 17.0   --Speed limit when 1 tire gets bursted
+local speedLimitDelay = 1000     --How long it takes (in milliseconds) for speed limit kicks in after tire bursts
 local vehicleSpeedMax = 0
 
 Citizen.CreateThread(function()
@@ -15,35 +16,35 @@ Citizen.CreateThread(function()
         
         if DoesEntityExist(veh) then
             vehicleSpeedMax = GetVehicleHandlingFloat(veh,"CHandlingData","fInitialDriveMaxFlatVel") -- Gets vehicle's maximum possible speed from handling file
-        end
---        RegisterCommand('burst', function()                   -- Debug command
---            SetVehicleTyreBurst(veh, 4, true, 1000.0)
---            StartVehicleAlarm(veh)
---            speed = GetEntitySpeed(veh)
---        end, false)
-		
+            --[[ RegisterCommand('burst', function() -- Debug command
+                SetVehicleTyreBurst(veh, 4, true, 1000.0)
+                StartVehicleAlarm(veh)
+                speed = GetEntitySpeed(veh)
+            end, false) ]]
         --------------------------------------------------------------------------
         ------Checks if any tyres are poped and adds it to popped tyres amount----
         --------------------------------------------------------------------------
         --Left Front
-        if IsVehicleTyreBurst(veh, 0, true)and tParams.t0 == false then
-            tParams.t0 = true
-            tParams.tyresPopped = tParams.tyresPopped + 1
-        end
-        --Right Front
-        if IsVehicleTyreBurst(veh, 1, true) and tParams.t1 == false then
-            tParams.t1 = true
-            tParams.tyresPopped = tParams.tyresPopped + 1
-        end
-        --Left Rear
-        if IsVehicleTyreBurst(veh, 4, true) and tParams.t4 == false then
-            tParams.t4 = true
-            tParams.tyresPopped = tParams.tyresPopped + 1
-        end
-        --Right Rear
-        if IsVehicleTyreBurst(veh, 5, true) and tParams.t5 == false then
-            tParams.t5 = true
-            tParams.tyresPopped = tParams.tyresPopped + 1
+            if IsVehicleTyreBurst(veh, 0, true)and tParams.t0 == false then
+                tParams.t0 = true
+                tParams.tyresPopped = tParams.tyresPopped + 1
+            end
+            --Right Front
+            if IsVehicleTyreBurst(veh, 1, true) and tParams.t1 == false then
+                tParams.t1 = true
+                tParams.tyresPopped = tParams.tyresPopped + 1
+            end
+            --Left Rear
+            if IsVehicleTyreBurst(veh, 4, true) and tParams.t4 == false then
+                tParams.t4 = true
+                tParams.tyresPopped = tParams.tyresPopped + 1
+            end
+            --Right Rear
+            if IsVehicleTyreBurst(veh, 5, true) and tParams.t5 == false then
+                tParams.t5 = true
+                tParams.tyresPopped = tParams.tyresPopped + 1
+            end
+
         end
 
         --Wheel id's on 6-wheeler: middle right - 3
@@ -53,11 +54,11 @@ Citizen.CreateThread(function()
         if tParams.tyresPopped >= 2 then
             local maxSpeedAfterBurst = speedLimitTwoTires
             
-            Wait(6000)
+            Wait(speedLimitDelay)
 
             while speedLimit >= maxSpeedAfterBurst do
                 SetVehicleMaxSpeed(veh, speedLimit)
-                speedLimit = speedLimit - 2.0
+                speedLimit = speedLimit - 4.0
                 Wait(200)
             end
 
@@ -95,4 +96,3 @@ Citizen.CreateThread(function()
 		end
     end
 end)
-
